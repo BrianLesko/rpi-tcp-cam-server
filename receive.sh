e44fd08b3dfcbdd28cd0392274db3ae6ea82236a
Brian Lesko <brianjosephlesko@gmail.com>
2024-11-02 14:22:31 -0400

    Update receive.sh

--- receive.sh
+++ receive.sh
@@ -5,4 +5,13 @@
 # 4/20/2024
 # receive the camera stream 
 
-ffplay tcp://172.20.10.3:8000 -vf "colorchannelmixer=rr=0:rb=1:br=1:bb=0, setpts=N/30" -fflags nobuffer -flags low_delay -framedrop
+# Check if an IP address was provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <IP_ADDRESS>"
+    exit 1
+fi
+
+# Use the provided IP address
+IP=$1
+
+ffplay tcp://$IP:8000 -vf "colorchannelmixer=rr=0:rb=1:br=1:bb=0, setpts=N/30" -fflags nobuffer -flags low_delay -framedrop
